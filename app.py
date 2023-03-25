631f4f5dbb2007b8c78121950876a76b25252b5c
Xayot0404 <pulatovxayot056@gmail.comgit config --global user.name Xayot0404git push -o origin main  git config --global user.email pulatovxayot056@gmail.com>
2023-03-25 16:59:31 +0500

    Modullar

---
+++ app.py
@@ -0,0 +1,51 @@
+# import random as r
+# import math as m
+# # 3.14 ozgarmas qiymat
+# # math medhods va MODULLAR 
+# # son = r.randint(1,100)
+# # print(son)
+# # ism = ["Bekzod","Hasan","Husan","Gani","Vali","Axror","Akil","Axil"]
+# # print(r.choice(ism))
+
+# # pi = m.pi
+# # e = m.pi
+# #floor en yaqin kichik butun songa yaxlitlidi
+# #ceil en yaqin kATTA butun songa yaxlitlidi
+# flr = m.floor(2.9)
+# cil = m.ceil(2.9)
+# #ildizdan chiqarish 
+# sqrt = m.sqrt(64) 
+# cbrt = m.cbrt(27) # kub ildiz osdi 
+# pw = m.pow(3,3) # darajaga kotarish 
+# trnc = m.trunc(3.3) # qoldiq kesadi trunc 
+# factrl = m.factorial(6)
+
+# print(flr)
+# print(cil)
+# print(sqrt)
+# print(cbrt)
+# print(pw)
+# print(trnc)
+# print(factrl)
+
+# son = (23)
+# print(m.trunc(m.sqrt(son)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
